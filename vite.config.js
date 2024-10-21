22fd3663f94c127c663b844a0cf0107ae6a82479
Kurobaka67 <M.arto@outlook.fr>
2024-10-21 15:44:47 +0200

    change vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,5 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/tds_download/" : "/",
+});
